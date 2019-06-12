21f361ae077d2f971e9f36c110446d17e42a830b
李超 <K.lee.9575@outlook.com>
2019-06-12 10:30:54 +0800

    add bernoulli_distribution

---
+++ bernoulli_distribution.go
@@ -0,0 +1,30 @@
+package mt19937
+
+import(
+	"log"
+)
+
+type bernoulli_distribution struct{
+	m_prob float64
+	m_eng *MT19937
+}
+
+func DistBernolli(eng *MT19937, prob float64) *bernoulli_distribution{
+	if(prob < 0 || prob > 1){
+		log.Println("prob must be between 0 and 1")
+		return nil
+	}
+	dist := &bernoulli_distribution{
+		m_prob : prob,
+		m_eng : eng,
+	}
+	return dist
+}
+
+func (dist *bernoulli_distribution) Bool() bool{
+	if (dist.m_prob == 0){
+		return false
+	}else{
+		return float64(dist.m_eng.Random()) <= dist.m_prob * float64(^uint64(0))
+	}
+}
